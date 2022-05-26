93391d411da438d36d9167999d2755bbb2e44fad
nofamex <nofaldi.atmam@tiket.com>
2022-05-26 22:02:52 +0700

    add logger

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"notification/database"
 	"notification/shared"
 	"notification/utils"
+	"os"
 
 	"github.com/streadway/amqp"
 )
 
 func main() {
+	f, err := os.OpenFile("log.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
 	utils.LoadConfig()
 
 	db, err:= database.NewDatabase()
